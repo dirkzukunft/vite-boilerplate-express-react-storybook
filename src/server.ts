4cca8207d027f667beca6a6a8fe4ae0ecd3186a6
dirkzukunft <git@qf3.de>
2021-08-24 11:51:27 +0200

    remove helmet due to storybook problems

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import helmet from 'helmet';
 dotenv.config();
 
 const port = process.env.EXPRESS_PORT || 3001;
 const app = express();
 app.use(express.json());
+app.disable('x-powered-by');
 
 app.use('/storybook', express.static('dist/storybook'));
 app.use(express.static('dist/app'));
-
-app.use(helmet());
-app.disable('x-powered-by');
 
 app.listen(port, async () => {
   console.log(`Listening at http://localhost:${port}`);
