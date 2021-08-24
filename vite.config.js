658b3e90024700d0917d263f26ff5c9ab50363ec
dirkzukunft <git@qf3.de>
2021-08-24 12:31:37 +0200

    Add basic API

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+const { PORT = 3001 } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
     outDir: 'dist/app',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+      },
+    },
   },
 });
