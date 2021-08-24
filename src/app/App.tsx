a0acd93d1c91049673ad45cb2df64f823305f04c
dirkzukunft <git@qf3.de>
2021-08-24 14:39:00 +0200

    Add routes to App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 export default function App(): JSX.Element {
-  return <>Hello!</>;
+  return (
+    <BrowserRouter>
+      Navigation
+      <Switch>
+        <Route path="/sample">
+          <main>sample page</main>
+        </Route>
+        <Route path="/">
+          <main>Home</main>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
