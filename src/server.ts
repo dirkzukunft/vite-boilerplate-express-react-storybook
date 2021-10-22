f72d0ccc4f00a87c83a5536c8afda22223f7c5cb
Dirk Zukunft <87307560+dirkzukunft@users.noreply.github.com>
2021-10-22 15:07:54 +0200

    Improve server.ts structure and prepare for mongoDB Connect (#15)

--- src/server.ts
+++ src/server.ts
@@ -9,17 +9,21 @@ app.disable('x-powered-by');
 
 app.use('/storybook', express.static('dist/storybook'));
 
-app.get('/api/', async (_req, res) => {
-  res.status(200).json({ message: 'API is running' });
-});
-
 app.use(express.static('dist/app'));
+// app.use('/api', myRoute);
+
 app.get('*', (_request, response) => {
   response.sendFile('index.html', { root: 'dist/app' });
 });
 
+// connectDb().then(
+// () => {
 app.listen(port, async () => {
+  console.log('Connected to MongoDB');
   console.log(`Frontend listening at http://localhost:3000`);
   console.log(`Backend listening at http://localhost:${port}`);
   console.log(`Storybook is at http://localhost:6006`);
 });
+// },
+// (error) => console.error(error)
+// );
